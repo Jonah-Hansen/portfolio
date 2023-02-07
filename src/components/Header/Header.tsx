b28a308a26940289e232866fc7a5efe0203ca53b
Jonah-Hansen <tce_mc@telus.net>
2023-02-06 19:36:54 -0600

    basic header styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,19 @@
+import styles from '@/styles/Header.module.scss';
 import Link from "next/link";
 import Logo from "../Logo/Logo";
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header} >
       <Link href='/' >
         <Logo />
       </Link>
-      <nav>
-        <ul>
-          <li><Link href='/' >HOME</Link></li>
-          <li><Link href='#about' >ABOUT</Link></li>
-          <li><Link href='#work' >WORK</Link></li>
-          <li><Link href='#contact' >CONTACT ME</Link></li>
+      <nav className={styles.nav} >
+        <ul className={styles.navList} >
+          <li><Link className={styles.navItem} href='/' >HOME</Link></li>
+          <li><Link className={styles.navItem} href='#about' >ABOUT</Link></li>
+          <li><Link className={styles.navItem} href='#work' >WORK</Link></li>
+          <li><Link className={styles.navItem} href='#contact' >CONTACT ME</Link></li>
         </ul>
       </nav>
     </header>
