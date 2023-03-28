27e179f5460f540f35f3d3dadba36699cab5e296
Jonah-Hansen <tce_mc@telus.net>
2023-03-27 19:34:09 -0600

    parallax and planets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import Contact from '@/components/Contact/Contact'
 import Header from '@/components/Header/Header'
 import Home from '@/components/Home/Home'
 import Work from '@/components/Work/Work'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function HomePage() {
   return (
@@ -15,11 +17,39 @@ export default function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <Header />
-        <Home />
-        <About />
-        <Work />
-        <Contact />
+        <Parallax pages={4} config={{ mass: 0 }} >
+          <ParallaxLayer sticky={{ start: 0, end: 3 }} >
+            <Header />
+          </ParallaxLayer>
+          <ParallaxLayer speed={-.9} factor={5} offset={-1} >
+            <div className='stars' />
+          </ParallaxLayer>
+
+          <ParallaxLayer speed={-.8}  >
+            <div className='planets' />
+          </ParallaxLayer>
+
+          <ParallaxLayer speed={-.4} >
+            <div className='big-planet' />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={0} >
+            <Home />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={1} >
+            <About />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={2} >
+            <Work />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={3} >
+            <Contact />
+          </ParallaxLayer>
+
+        </Parallax>
       </main>
     </>
   )
