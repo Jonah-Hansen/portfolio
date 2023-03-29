cbe9e075b7539bde78f3ca59d273b1fd35d9a968
Jonah-Hansen <tce_mc@telus.net>
2023-03-28 20:42:31 -0600

    remove react-spring parallax. inconsistent across browsers going to try manual parallax

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useRef, useState } from "react";
 
 export default function About() {
+  // const [clipPath, setClipPath] = useState<string>('polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)');
+
+  // useEffect(() => {
+  //   function handleScroll() {
+  //     const element = clipRef.current;
+  //     console.log(element)
+  //     if (!element) return;
+
+  //     const elementOffset = element.offsetTop;
+  //     const scrollPosition = window.scrollY;
+  //     const viewportHeight = window.innerHeight;
+  //     const distanceFromViewportTop = elementOffset - scrollPosition;
+  //     const clipPathHeight = Math.max(0, viewportHeight - distanceFromViewportTop);
+  //     const percentage = clipPathHeight / viewportHeight;
+  //     setClipPath(`polygon(0% ${percentage * 100}%, 100% ${percentage * 100}%, 100% 100%, 0% 0%)`);
+  //   }
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, []);
+
+  // const clipRef = useRef<HTMLElement>(null);
+
+  // style={{ clipPath }} ref={clipRef}
+
   return (
-    <section>
+    <section >
       <h2>About Me</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium cupiditate commodi, facere corporis vel culpa et hic cum. Quo quia vel, nemo repellat mollitia nobis id fuga vitae veniam aliquam.</p>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,7 @@ import Contact from '@/components/Contact/Contact'
 import Header from '@/components/Header/Header'
 import Home from '@/components/Home/Home'
 import Work from '@/components/Work/Work'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function HomePage() {
   return (
@@ -17,39 +15,22 @@ export default function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <Parallax pages={4} config={{ mass: 0 }} >
-          <ParallaxLayer sticky={{ start: 0, end: 3 }} >
-            <Header />
-          </ParallaxLayer>
-          <ParallaxLayer speed={-.9} factor={5} offset={-1} >
-            <div className='stars' />
-          </ParallaxLayer>
 
-          <ParallaxLayer speed={-.8}  >
-            <div className='planets' />
-          </ParallaxLayer>
+        <Header />
 
-          <ParallaxLayer speed={-.4} >
-            <div className='big-planet' />
-          </ParallaxLayer>
+        <div className='stars' />
 
-          <ParallaxLayer offset={0} >
-            <Home />
-          </ParallaxLayer>
+        <div className='planets' />
 
-          <ParallaxLayer offset={1} >
-            <About />
-          </ParallaxLayer>
+        <div className='big-planet' />
 
-          <ParallaxLayer offset={2} >
-            <Work />
-          </ParallaxLayer>
+        <Home />
 
-          <ParallaxLayer offset={3} >
-            <Contact />
-          </ParallaxLayer>
+        <About />
 
-        </Parallax>
+        <Work />
+
+        <Contact />
       </main>
     </>
   )
