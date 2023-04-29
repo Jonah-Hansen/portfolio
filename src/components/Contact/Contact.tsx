9abb6aa09651253b8c1dba9205f25d7dde751297
Jonah-Hansen <tce_mc@telus.net>
2023-04-28 18:21:54 -0600

    add contact links

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
   return (
     <section id='contact' className={styles.contact} style={{ WebkitMaskImage }} ref={clipRef} >
       <h2>Contact Me!</h2>
-      <form className={styles.form} >
+      {/* <form className={styles.form} >
         <label >
           Email:
           <input type="email" placeholder='your email here' />
@@ -21,7 +21,28 @@ export default function Contact() {
           <textarea placeholder='your message here' />
         </label>
         <button>SEND</button>
-      </form>
+      </form> */}
+
+      <div className={styles.links}>
+        <div className={styles.link}>
+          <p>find me on Linkedin at:</p>
+          <a target='_blank' rel='noopener noreferrer' href={'https://linkedin.com/in/jonah-hansen-dev'}  >
+            linkedin.com/in/jonah-hansen-dev
+          </a>
+        </div>
+        <div className={styles.link}>
+          <p>or send me an email at:</p>
+          <a href={'mailto:jonah.m.hansen@gmail.com'}  >
+            jonah.m.hansen@gmail.com
+          </a>
+        </div>
+        <div className={styles.link}>
+          <p>Click here to see my resume:</p>
+          <a target='_blank' rel='noopener noreferrer' href={'/Jonah-Hansen-Resume.pdf'}  >
+            Jonah-Hansen-Resume.pdf
+          </a>
+        </div>
+      </div>
     </section>
   )
 }
